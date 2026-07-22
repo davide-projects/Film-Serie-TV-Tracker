8692cddc3a585a4c34de0ab8008228f111320d2b
Davide <davide.barbieri1@outlook.it>
2026-07-22 10:41:51 +0200

    Upgrade: Add Interfaces

---
+++ src/app/models/models.ts
@@ -0,0 +1,21 @@
+export interface Genre {
+    id: number;
+    name: string;
+    emoji?: string
+}
+
+export interface Title {
+    id: number;
+    title: string;
+    type: 'movie' | 'series';
+    year: number;
+    duration: number;
+    rate: number;
+    description?: string;
+}
+
+export interface ListItem {
+    title: 'Title';
+    state: 'watched' | 'to-watch' | 'favorite';
+    releaseDate?: string
+}
